f11ea1f65724ef6850c488f874b8747be8552276
harith md noor <harithmdnoor@gmail.com>
2021-05-27 23:54:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from selenium import webdriver
 import time 
 import re
 app = Flask(__name__)
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 @app.route("/")
 def home():
