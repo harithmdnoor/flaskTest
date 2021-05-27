dacd3af017b25639224a28eaa386d006000f5dbf
harith md noor <harithmdnoor@gmail.com>
2021-05-28 01:40:48 +0800

    dsa

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask , render_template, request
 from datetime import datetime, timedelta
-from selenium import webdriver
 import time 
-import os
+import requests
 import re
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
@@ -13,64 +13,55 @@ def home():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    username = request.form['username']
-    password = request.form['password']
-    data = checkPay(username,password)
+    payload={
+        'userId': request.form['username'],
+        'password': request.form['password'],
+        'Cluster_Id':'8'
+    }
+    data = checkPay(payload)
     return render_template('hello_there.html',data = data)
 
-def checkPay(userInput,passInput):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
-    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+def checkPay(payload):
+   
     todayDate = datetime.today()
     yesterdayDate = todayDate - timedelta(days=1)
-    url1 = "https://abs.rafflesmedical.com.sg/eroster/Account/Logout"
+    url1 = "https://abs.rafflesmedical.com.sg/eroster/Account/Login"
 
     url2 ="https://abs.rafflesmedical.com.sg/eroster/Attendance/History?DateFrom={}&DateTo={}".format("01-March-2021",yesterdayDate.strftime("%d-%B-%Y"))
-    browser.get(url1)
 
-    usernameInput = browser.find_element_by_id('editorUserName')
-    usernameInput.send_keys(userInput)
-    passwordInput= browser.find_element_by_id("password")
-    passwordInput.send_keys(passInput)
-    locationInput = browser.find_element_by_id('ddlClusters')
-    for option in locationInput.find_elements_by_tag_name('option'):
-        if option.text == "Pasir Ris Elias (PRE)":
-            option.click()
-    submitBtn = browser.find_element_by_class_name("btn-info")
-    submitBtn.click()
-    time.sleep(1)
-    browser.get(url2)
-    workHistory = browser.find_element_by_class_name("list-group")
-    shiftList =[]
-    ciList =[]
-    coList=[]
-
-    for row in workHistory.find_elements_by_xpath('li'):
-        if row.text.split("\n")[1] == "Pasir Ris Elias":
-
-            shiftList.append(row.text.split("\n")[0])
-            ciList.append(datetime.strptime(row.text.split("\n")[2][4:],"%H:%M"))
-            coList.append(datetime.strptime(row.text.split("\n")[-1][4:],"%H:%M"))
-        aprilPay=0.0
-        mayPay=0
-        junePay=0
-        julyPay=0
-        for i in range(len(shiftList)):
-            if shiftList[i].split(" ")[2]=="Apr":
-                aprilPay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
-            elif shiftList[i].split(" ")[2]=="May":
-                mayPay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
-            elif shiftList[i].split(" ")[2]=="Jun":
-                junePay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
-    browser.close()
-    returnVal ={"username":userInput,
-                "aprilPay": aprilPay,
-                "mayPay": mayPay,
-                "junePay":junePay}
-
+    # Use 'with' to ensure the session context is closed after use.
+    with requests.Session() as s:
+        p = s.post(url1, data=payload)
+        # print the html returned or something more intelligent to see if it's a successful login page.
+        r = s.get(url2)
+        soup = BeautifulSoup(r.text,"lxml")
+        # print(workHistory)
+        shiftList =[]
+        ciList =[]
+        coList=[]
+        
+        for row in soup.find_all("li",class_="list-group-item"):
+            if row.text.split('\n')[4] == " Pasir Ris Elias":
+                shiftList.append(row.text.split("\n")[3])
+                ciList.append(datetime.strptime(row.text.split("\n")[10],"%H:%M"))
+                coList.append(datetime.strptime(row.text.split("\n")[14],"%H:%M"))
+            aprilPay=0.0
+            mayPay=0
+            junePay=0
+            julyPay=0
+            
+            for i in range(len(shiftList)):
+                if shiftList[i].split(" ")[2]=="Apr":
+                    aprilPay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
+                elif shiftList[i].split(" ")[2]=="May":
+                    mayPay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
+                elif shiftList[i].split(" ")[2]=="Jun":
+                    junePay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
+                elif shiftList[i].split(" ")[2]=="July":
+                    julyPay+=(((coList[i]-ciList[i])-timedelta(hours=1)).total_seconds()/3600)*13
+    returnVal ={"username":payload.get("userId"),
+                    "aprilPay": aprilPay,
+                    "mayPay": mayPay,
+                    "junePay":junePay,
+                    "julyPay":julyPay}
     return returnVal
